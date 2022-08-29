8f9f5026572904bd5f2f8f3ae6fcf1f326a3497c
Wellington Defassio <well.defassio@gmail.com>
2022-08-29 15:15:23 -0300

    initial commid

--- src/main/java/com/forum/ForumApplication.java
+++ src/main/java/com/forum/ForumApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ForumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForumApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ForumApplication.class, args);
+    }
 
 }
--- src/main/java/com/forum/controller/HelloController.java
+++ src/main/java/com/forum/controller/HelloController.java
@@ -1,2 +1,16 @@
-package com.forum.controller;public class HelloController {
+package com.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello(){
+        return "hello word";
+    }
 }
